d20068fab71b74642d1aaf258e00c6598420d417
Siddhartha Dutta <siddhartha_dutta@live.com>
2020-05-04 01:06:16 +0530

    refactored blockproduction test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <map>
 
-#include "../core/Simulator/Simulator.h"
+#include "SubnetTest.h"
 #include "../helpers/Logger/easylogging.h"
 
 using namespace std;
@@ -20,51 +20,13 @@ void configureLogger() {
     el::Loggers::reconfigureLogger("networkTopology", networkTopologyConf);
 }
 
-bool checkBlockTimeMean(Simulator &simulator) {
-	//    simulator.start();
-	long long sum = 0;
-	for (long long i = 0; i < 1000000; i++) {
-		int x = simulator.getBlockchainManagementModel()->getNextBlockTime();
-//		    	cout<<x<<endl;
-		sum += x;
-	}
-
-	double avg = sum / 1000000;
-	cout << "Avg: " << avg << endl;
-
-	return (avg > 600 - 10) && (avg < 600 + 10);
-}
-
-bool checkBlockProducerDistribution(Simulator &simulator) {
-	unordered_map<int, int> m;
-
-	for (long long i = 0; i < 100000; i++) {
-		int x = simulator.getBlockchainManagementModel()->getNextBlockProducerId();
-		if(m.find(x) == m.end()) {
-			m.insert(make_pair(x, 1));
-		}
-		else m[x]++;
-	}
-
-	for(auto it: m) {
-		cout<<it.first<<" "<<it.second<<endl;
-	}
-
-	return true;
-}
-
 int main() {
 	configureLogger();
 
 	LOG(INFO) << "[Test started]";
 	
-	Simulator simulator;
-    simulator.setup();
-//    simulator.start();
-
-//	checkBlockTimeMean(simulator);
-
-	checkBlockProducerDistribution(simulator);
+	SubnetTest st;
+	st.test();
 
     LOG(INFO) << "[Test stopped]";
 
