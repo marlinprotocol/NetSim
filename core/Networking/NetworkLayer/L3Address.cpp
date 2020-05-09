1e0b76759d3165972022e36d093888ee3ed06ac5
Siddhartha Dutta <siddhartha_dutta@live.com>
2020-05-09 11:39:35 +0530

    updated L3Address

--- core/Networking/NetworkLayer/L3Address.cpp
+++ core/Networking/NetworkLayer/L3Address.cpp
@@ -1,1 +1,14 @@
+#include <functional>
+
 #include "L3Address.h"
+
+namespace std
+{
+    template<> struct hash<L3Address>
+    {
+    	std::size_t operator()(L3Address const& l) const noexcept
+        {
+            return 0; // TODO
+        }
+    };
+}
